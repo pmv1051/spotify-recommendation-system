34cf7aa36680e423f1a269fc74466fea292ddd1e
Jenish Patel <jpatel102005@gmail.com>
2026-03-26 23:04:56 -0400

    finished splay tree implementation

--- Splay_Tree.py
+++ Splay_Tree.py
@@ -1,4 +1,11 @@
 # Splay Tree implementation by Jenish
+# Includes top_k and insert integrations into the benchmark 
+
+# Resources used:
+# 
+#
+
+
 
 class Node:
     def __init__(self, score, index):
@@ -21,15 +28,142 @@ class SplayTree:
     def insert(self, score, index):
         #insert logic
 
-    def left_rotate(self, x):
+        node = Node(score, index)
+
+        if self.root is None:
+            self.root = node
+            return
+        
+        #traverse down BST insert
+        c = self.root
+
+        start = True
+
+        while start:
+            if (score, index) < (c.score, c.index):
+                if c.left is None:
+                    c.left = node
+                    node.parent  = c
+
+                    start = False
+                    break
+                c = c.left
+            
+            else:
+                if c.right is None:
+                    c.right = node
+                    node.parent = c
+
+                    start = False
+                    break
+
+                c = c.right
+        
+        self.splay(node)
+
+
+    def left_rotate(self, x: Node):
         #left rotation logic
+        if x is None or x.right is None:
+            return
+        
+        y = x.parent
+        right = x.right
+
+        x.right = right.left
+        if right.left is not None:
+            right.left.parent = x
+
+        right.parent = y
+        
+        if y is None: 
+            self.root = right
+
+        else:
+            if y.left == x:
+                y.left = right
+
+            else:
+                y.right = right
+
+        right.left = x
+        x.parent = right
+            
 
     def right_rotate(self, x):
         #right rotation logic
+        if x is None or x.left is None:
+            return 
+        
+        y = x.parent
+        left = x.left
+
+        x.left = left.right
+
+        if left.right is not None:
+            left.right.parent = x
+
+        left.parent = y
+        if y is None: 
+            self.root = left
+
+        else: 
+            if y.right == x:
+                y.right = left
+
+            else: 
+                y.left = left
+        
+        left.right = x
+        x.parent = left
+
+        
+
+    def sort_inorder(self, node, result, k):
+        if node is None or len(result) >= k:
+            return
+
+        #resursive right for largest val
+        self.sort_inorder(node.right, result, k)
+
+        if len(result) < k:
+            result.append((node.score, node.index))
+
+        #recursive left
+        self.sort_inorder(node.left, result, k)
 
     def top_k(self, k):
+        result = []
+        self.sort_inorder(self.root, result, k)
+
+        return result
+    
 
     def splay(self, x):
+ 
+        while x.parent is not None:
 
-    
-    
+            #zig 
+            if x.parent.parent is None:
+                if x == x.parent.left:
+                    self.right_rotate(x.parent)
+                else: 
+                    self.left_rotate(x.parent)
+
+            #zigzig
+            elif x == x.parent.left and x.parent == x.parent.parent.left:
+                self.right_rotate(x.parent.parent)
+                self.right_rotate(x.parent)
+
+            elif x == x.parent.right and x.parent == x.parent.parent.right:
+                self.left_rotate(x.parent.parent)
+                self.left_rotate(x.parent)
+            
+            #zigzag
+            elif x == x.parent.right and x.parent == x.parent.parent.left:
+                self.left_rotate(x.parent)
+                self.right_rotate(x.parent)
+
+            elif x == x.parent.left and x.parent == x.parent.parent.right:
+                self.right_rotate(x.parent)
+                self.left_rotate(x.parent)
